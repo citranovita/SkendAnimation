a2a396adfaf2cf2d5f231ab11e178ed5fbaa5923
citranovita <128466402+citranovita@users.noreply.github.com>
2024-09-23 19:11:16 +0800

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,4 +23,516 @@ class UserSeeder extends Seeder
             'updated_at' => now(),
         ]);
     }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AGHNIA VALERINA ALI',
+            'status' => 'siswa',
+            'nis' => 11464,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AHMAD NIZAR',
+            'status' => 'siswa',
+            'nis' => 11465,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AHMAD RIYADI SULAIMAN',
+            'status' => 'siswa',
+            'nis' => 11466,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AKHMAD AZMI LUTHFI',
+            'status' => 'siswa',
+            'nis' => 11467,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'ANNISA QANITA NAJIYAH',
+            'status' => 'siswa',
+            'nis' => 11468,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AURELLIA MAULIDA PASHA',
+            'status' => 'siswa',
+            'nis' => 11469,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'BAYU ADJIE DARMA SANTIKO',
+            'status' => 'siswa',
+            'nis' => 11470,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'DINDA MAHARANI',
+            'status' => 'siswa',
+            'nis' => 11471,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'ERFANDI',
+            'status' => 'siswa',
+            'nis' => 11472,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'FATIMAH AZZAHRA',
+            'status' => 'siswa',
+            'nis' => 11473,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'HENDRI',
+            'status' => 'siswa',
+            'nis' => 11474,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'ISMI MAULANI HAFIZAH',
+            'status' => 'siswa',
+            'nis' => 11475,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'KINDY NURCHOLIFAH',
+            'status' => 'siswa',
+            'nis' => 11476,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'LUKMAN HAKIM',
+            'status' => 'siswa',
+            'nis' => 11477,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'M. FAHRI HANIFA',
+            'status' => 'siswa',
+            'nis' => 11478,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MOH. SYAHID ALI NURDIN',
+            'status' => 'siswa',
+            'nis' => 11479,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD AL FATIH',
+            'status' => 'siswa',
+            'nis' => 11480,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD ARGA',
+            'status' => 'siswa',
+            'nis' => 11481,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD AS AD',
+            'status' => 'siswa',
+            'nis' => 11482,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD ISMAIL',
+            'status' => 'siswa',
+            'nis' => 11483,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD LUTFI',
+            'status' => 'siswa',
+            'nis' => 11484,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD RADHITYA RAHMAN',
+            'status' => 'siswa',
+            'nis' => 11485,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD RAIHAN NABIL',
+            'status' => 'siswa',
+            'nis' => 11486,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD RAMADHANI',
+            'status' => 'siswa',
+            'nis' => 11487,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD RIYANTO PUTRA PRATAMA',
+            'status' => 'siswa',
+            'nis' => 11488,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD ROSSI PERMANA',
+            'status' => 'siswa',
+            'nis' => 11489,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD YUSUF SULAIMAN',
+            'status' => 'siswa',
+            'nis' => 11490,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'NICOLE SUGIANTO',
+            'status' => 'siswa',
+            'nis' => 11491,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'NIZAR SAPUTRA',
+            'status' => 'siswa',
+            'nis' => 11492,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'RAHMAD DENI RAMADHAN',
+            'status' => 'siswa',
+            'nis' => 11493,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'RIANTI NUR A IN',
+            'status' => 'siswa',
+            'nis' => 11494,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'RISKA SAFITRI',
+            'status' => 'siswa',
+            'nis' => 11495,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'SEPTIYA',
+            'status' => 'siswa',
+            'nis' => 11496,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'SRI MAHARANI',
+            'status' => 'siswa',
+            'nis' => 11497,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => '',
+            'status' => 'siswa',
+            'nis' => ,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'SUCI RAHMAWATI',
+            'status' => 'siswa',
+            'nis' => 11498,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'VIVI ALAYYDA ANDINI',
+            'status' => 'siswa',
+            'nis' => 10741,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AHMAD RIDANI',
+            'status' => 'siswa',
+            'nis' => 11499,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AILUL FEBRIANNUR',
+            'status' => 'siswa',
+            'nis' => 11500,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AMMA RASYAD ADVIENDRA',
+            'status' => 'siswa',
+            'nis' => 11501,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'ANISA FITRI',
+            'status' => 'siswa',
+            'nis' => 11502,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'AURA DIVIYA ELZAHRA',
+            'status' => 'siswa',
+            'nis' => 11503,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'BINTANG ARYA ZUMAR RAMADHAN',
+            'status' => 'siswa',
+            'nis' => 11504,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'CAHYO BANGKIT WIJAYA',
+            'status' => 'siswa',
+            'nis' => 11505,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'CLAUDIA ANGGITA HARTONO',
+            'status' => 'siswa',
+            'nis' => 11506,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'ELISKA KUMALA SARI',
+            'status' => 'siswa',
+            'nis' => 11507,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'FLORA JANNATI',
+            'status' => 'siswa',
+            'nis' => 11508,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'HERLING YAKINAS RAMOPOLI',
+            'status' => 'siswa',
+            'nis' => 11509,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'JHARA DELLA PUSPITA',
+            'status' => 'siswa',
+            'nis' => 11510,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'M. ARSYAD MAULANA',
+            'status' => 'siswa',
+            'nis' => 11511,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'M. IBNU FIRDAUS',
+            'status' => 'siswa',
+            'nis' => 11512,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MEYSA AL JIHAN',
+            'status' => 'siswa',
+            'nis' => 11513,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMAD GALANG SYAPUTRA',
+            'status' => 'siswa',
+            'nis' => 11514,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD ALQA AL HAYYU',
+            'status' => 'siswa',
+            'nis' => 11515,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD ARIF MUZAKY',
+            'status' => 'siswa',
+            'nis' => 11516,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD HAFIS',
+            'status' => 'siswa',
+            'nis' => 11517,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD ISRO ABDI',
+            'status' => 'siswa',
+            'nis' => 11518,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD NABIEL INDRAWAN',
+            'status' => 'siswa',
+            'nis' => 11519,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD RAHMAN',
+            'status' => 'siswa',
+            'nis' => 11520,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD RAJU MAULANA',
+            'status' => 'siswa',
+            'nis' => 11521,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD REFANO MAULANA',
+            'status' => 'siswa',
+            'nis' => 11522,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD RIZALDI RADITYA',
+            'status' => 'siswa',
+            'nis' => 11523,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD YUDI PRATAMA',
+            'status' => 'siswa',
+            'nis' => 11524,
+        ]);
+    }
+     DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'MUHAMMAD ZULFIKAR HASIBUAN',
+            'status' => 'siswa',
+            'nis' => 11525,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'NURUL HIDAYATULLAH',
+            'status' => 'siswa',
+            'nis' => 11526,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'PETER LEVI SAHALA SITOMPUL',
+            'status' => 'siswa',
+            'nis' => 11527,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'RIF AH SRI AMALIA',
+            'status' => 'siswa',
+            'nis' => 11528,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'RIFNU FAJAR',
+            'status' => 'siswa',
+            'nis' => 11529,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'SAUQI PRATAMA',
+            'status' => 'siswa',
+            'nis' => 11530,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'SITI MASITOH',
+            'status' => 'siswa',
+            'nis' => 11531,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'ST. FATMA SHOLEHAH',
+            'status' => 'siswa',
+            'nis' => 11532,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'SYIVA AMALIA ANSARI',
+            'status' => 'siswa',
+            'nis' => 11533,
+        ]);
+    }
+ DB::table('users')->insert([
+            'user_id' => null,
+            'nama' => 'VINA NABILLA',
+            'status' => 'siswa',
+            'nis' => 11534,
+        ]);
+    }
+}
 }
